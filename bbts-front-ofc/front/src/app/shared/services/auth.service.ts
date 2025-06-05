2b2f369ba4deba871ac4232a5e1a85a43cb2d0c1
Direlly <direllybarbosa@gmail.com>
2025-06-05 06:35:57 -0300

    vaii

--- bbts-front-ofc/front/src/app/shared/services/auth.service.ts
+++ bbts-front-ofc/front/src/app/shared/services/auth.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private baseUrl = 'http://localhost:8080'; 
+  private baseUrl = 'http://localhost:8080/auth'; 
 
   constructor(private http: HttpClient, private router: Router) {}
 
   login(cpf: string, senha: string): Observable<any> {
-    return this.http.post(`${this.baseUrl}/auth/login`, { cpf, senha });
+    return this.http.post(`${this.baseUrl}/login`, { username: cpf, password: senha }).pipe(
+      tap((response: any) => {
+        if (response.token) {
+          localStorage.setItem('token', response.token);
+        }
+      })
+    );
   }
 
   register(nome: string, email: string, cpf: string, senha: string): Observable<any> {
-    return this.http.post(`${this.baseUrl}/auth/register`, { nome, email, cpf, senha });
+    return this.http.post(`${this.baseUrl}/register`, { 
+      nome, 
+      email, 
+      cpf, 
+      senha 
+    }).pipe(
+      tap((response: any) => {
+        if (response.token) {
+          localStorage.setItem('token', response.token);
+        }
+      })
+    );
   }
 
   logout(): void {
